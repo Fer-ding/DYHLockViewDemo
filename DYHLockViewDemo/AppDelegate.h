51d5740d3fcec7681e975188732d559cce6246a6
LeapDing <2390487566@qq.com>
2016-12-23 14:09:05 +0800

    Initial Commit

---
+++ DYHLockViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYHLockViewDemo
+//
+//  Created by YueHui on 16/12/23.
+//  Copyright © 2016年 LeapDing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYHLockViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYHLockViewDemo
+//
+//  Created by YueHui on 16/12/23.
+//  Copyright © 2016年 LeapDing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
